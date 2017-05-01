555ed8d0274fcad55b76a692e48b97702e18aa44
libaniocv <libaniocv@gmail.com>
2017-05-01 12:58:53 -0300

    first commit

---
+++ main/forms.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django import forms
+from . import models
+
+
+##USUARIOS
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = models.User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = models.UserPerfil
+        fields = ('curriculos', 'foto')
